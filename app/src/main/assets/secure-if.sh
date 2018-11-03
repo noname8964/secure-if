7a9a6676deb3d727a9a2fb9da8d72bbbe35844fb
noname8964 <32409462+noname8964@users.noreply.github.com>
2018-11-03 17:15:08 +0800

    Update boot script

--- app/src/main/assets/secure-if.sh
+++ app/src/main/assets/secure-if.sh
@@ -1,6 +1,19 @@
 #!/system/bin/sh
 
 setprop persist.sys.usb.config none
+setprop sys.usb.config.fac none
 setprop sys.usb.config none
 setprop sys.usb.configfs 0
-start adbd
+
+boot=$(getprop sys.boot_completed)
+while [ $boot = "0" ]; do
+  sleep 10
+  boot=$(getprop sys.boot_completed)
+  if [ $boot = "1" ]; then
+    setprop persist.sys.usb.config none
+    setprop sys.usb.config.fac none
+    setprop sys.usb.config none
+    setprop sys.usb.configfs 0
+    #start adbd
+  fi
+done
