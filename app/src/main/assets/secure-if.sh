7a909316d73bc42b6cbd0ccdcb33400b541663ce
noname8964 <32409462+noname8964@users.noreply.github.com>
2018-11-22 17:53:25 +0800

    Update boot script

--- app/src/main/assets/secure-if.sh
+++ app/src/main/assets/secure-if.sh
@@ -5,15 +5,17 @@ setprop sys.usb.config.fac none
 setprop sys.usb.config none
 setprop sys.usb.configfs 0
 
-boot=$(getprop sys.boot_completed)
-while [ $boot = "0" ]; do
+boot=$(getprop sys.usb.state)
+while [ $boot = "none" ]; do
   sleep 10
-  boot=$(getprop sys.boot_completed)
-  if [ $boot = "1" ]; then
+  boot=$(getprop sys.usb.state)
+  if [ $boot != "none" ]; then
+    settings put global adb_enabled 0
     setprop persist.sys.usb.config none
     setprop sys.usb.config.fac none
     setprop sys.usb.config none
     setprop sys.usb.configfs 0
+    break
     #start adbd
   fi
 done
